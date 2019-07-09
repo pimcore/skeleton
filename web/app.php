d1613c4d3b5a74e9dbd66b7f4c94c131fbd8158e
t.pozzo <t.pozzo@agence-senzo.fr>
2019-07-10 00:36:00 +0200

    New .env process

--- web/app.php
+++ web/app.php
@@ -16,7 +16,14 @@ use Pimcore\Tool;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-include __DIR__ . "/../vendor/autoload.php";
+require dirname(__DIR__).'/app/bootstrap.php';
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
 
 \Pimcore\Bootstrap::setProjectRoot();
 \Pimcore\Bootstrap::boostrap();
