067de35ebc629607eefc5a5240fe61a7c83f908f
brusch <bernhard.rusch@elements.at>
2020-11-18 13:05:36 +0100

    Pimcore 7 fixes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,10 +4,17 @@ namespace AppBundle\Controller;
 
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends FrontendController
 {
+    /**
+     * @Template
+     * @param Request $request
+     * @return array
+     */
     public function defaultAction(Request $request)
     {
+        return [];
     }
 }
