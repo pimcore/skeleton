3dfcf6df022e9302cab4f038f2a4e382234e66f1
BlackbitNeueMedien <jan.walther@blackbit.de>
2021-08-11 17:46:51 +0200

    Redirect to Pimcore backend by default

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,17 +4,27 @@ namespace App\Controller;
 
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends FrontendController
 {
     /**
+     * @param Request $request
+     * @return Response
+     */
+    public function defaultAction(Request $request)
+    {
+        return $this->redirect('/admin');
+    }
+    
+    /**
      * @Template
      * @param Request $request
      * @return array
      */
-    public function defaultAction(Request $request)
+    public function contentAction(Request $request)
     {
         return [];
     }
