90ffc98fe6d78f8370a979b39d6f4a58ee30ab80
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2022-07-29 20:27:52 +0200

    Add a basic unit test with a mock

---
+++ tests/unit/Controller/DefaultControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Controller;
+
+use App\Controller\DefaultController;
+use Codeception\Test\Unit;
+use PHPUnit\Framework\MockObject\MockObject;
+use Pimcore\Templating\TwigDefaultDelegatingEngine;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\Environment;
+
+/**
+ * This basic unit test demonstrates how to unit-test a controller using mocked twig engine.
+ */
+class DefaultControllerTest extends Unit
+{
+    private DefaultController $controller;
+
+    /** @var MockObject|Environment  */
+    private MockObject $twig;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Create a test double for twig engine.
+        // See: https://phpunit.readthedocs.io/en/9.5/test-doubles.html
+        $this->twig = $this->createMock(Environment::class);
+
+        $container = new Container();
+        $container->set('twig', $this->twig);
+        $container->set('pimcore.templating', new TwigDefaultDelegatingEngine($this->twig, []));
+
+        $this->controller = new DefaultController();
+        $this->controller->setContainer($container);
+    }
+
+    public function testDefaultAction()
+    {
+        $this->twig->method('render')->will(
+            $this->returnValueMap([
+                // Simulate rendering of default template.
+                ['default/default.html.twig', [], 'Welcome to awesome PimCore!']
+            ])
+        );
+
+        $response = $this->controller->defaultAction($this->createMock(Request::class));
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertStringContainsStringIgnoringCase('welcome', $response->getContent());
+        self::assertStringContainsStringIgnoringCase('pimcore', $response->getContent());
+        self::assertStringContainsStringIgnoringCase('awesome', $response->getContent());
+        self::assertStringNotContainsStringIgnoringCase('bugs', $response->getContent());
+    }
+}
