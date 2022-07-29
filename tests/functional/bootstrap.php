ffb33bd0bf11b988cf8c3745e0dc6ead177a3506
Iaroslav Sheptykin <yarik.sheptykin@gmail.com>
2022-07-29 19:22:43 +0200

    Explain clearly
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

--- tests/functional/bootstrap.php
+++ tests/functional/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-// We want to use the global bootstrap, but customize the setup a bit for running a wider-scope tests.
+// We want to use the global bootstrap, but customize the setup a bit to run tests with a larger scope.
 require_once dirname(__DIR__) . '/bootstrap.php';
 
 // Let tests believe that the process is executed as a console application.
