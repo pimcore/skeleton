1224521e1af32aa013072cf4a64d30275e980ae0
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2022-12-28 13:39:03 +0100

    [Runtime] use Symfony Runtime

--- public/index.php
+++ public/index.php
@@ -1,39 +1,25 @@
 <?php
 
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Enterprise License (PEL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PEL
- */
-
+use Pimcore\Bootstrap;
 use Pimcore\Tool;
 use Symfony\Component\HttpFoundation\Request;
 
-include __DIR__ . "/../vendor/autoload.php";
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-\Pimcore\Bootstrap::setProjectRoot();
-\Pimcore\Bootstrap::bootstrap();
+Bootstrap::setProjectRoot();
+Bootstrap::bootstrap();
 
-$request = Request::createFromGlobals();
+return function (Request $request, array $context) {
 
-// set current request as property on tool as there's no
-// request stack available yet
-Tool::setCurrentRequest($request);
+    // set current request as property on tool as there's no
+    // request stack available yet
+    Tool::setCurrentRequest($request);
 
-/** @var \Pimcore\Kernel $kernel */
-$kernel = \Pimcore\Bootstrap::kernel();
+    /** @var \Pimcore\Kernel $kernel */
+    $kernel = \Pimcore\Bootstrap::kernel();
 
-// reset current request - will be read from request stack from now on
-Tool::setCurrentRequest(null);
+    // reset current request - will be read from request stack from now on
+    Tool::setCurrentRequest(null);
 
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+    return $kernel;
+};
