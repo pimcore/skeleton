adc9dd02d0e09c62af96f5a23f32a3da60823d2c
mattamon <matthias.schuhmayer@pimcore.com>
2023-05-12 16:42:56 +0200

    Remove dev repos

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
 ];
