0c70d15c32aef408117c5a00b3354464e8446c24
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2023-05-12 16:51:27 +0200

    [Task] Add BundleSetupSubscriber (#149)
    * Add BundleSetupSubscriber
    
    * Adding AdminBundle
    
    * Require AdminBundle
    
    * add excluded from bundles.php flag
    
    * Add RC1 conflict
    
    * Remove dev repos
    
    * Fix wrong files

---
+++ src/EventSubscriber/BundleSetupSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace App\EventSubscriber;
+
+use Pimcore\Bundle\AdminBundle\PimcoreAdminBundle;
+use Pimcore\Bundle\InstallBundle\Event\BundleSetupEvent;
+use Pimcore\Bundle\InstallBundle\Event\InstallEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class BundleSetupSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            InstallEvents::EVENT_BUNDLE_SETUP => [
+                ['bundleSetup'],
+            ],
+        ];
+    }
+
+    public function bundleSetup(BundleSetupEvent $event): void
+    {
+        // add required PimcoreAdminBundle
+        $event->addRequiredBundle('PimcoreAdminBundle', PimcoreAdminBundle::class, true);
+    }
+}
