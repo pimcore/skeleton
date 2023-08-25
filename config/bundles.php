05b7fa3ec3ed0862720b4e5736f831b5f837f3f9
Hasan Elahi <helahi@keh.com>
2023-08-25 11:50:49 -0400

    Install data importer

--- config/bundles.php
+++ config/bundles.php
@@ -3,3 +3,14 @@
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
+
+use Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle;
+// ...
+
+return [
+    // ...
+    // make sure PimcoreDataHubBundle is added before to that list
+    // ...
+    PimcoreDataImporterBundle::class => ['all' => true],
+    // ...
+];
