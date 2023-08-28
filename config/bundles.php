1b40f48aee644af2635b655f16c00b6e33b04928
Hasan Elahi <helahi@keh.com>
2023-08-28 11:56:49 -0400

    logger bundle add.

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,11 @@
 
 use Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle;
 use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
+use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 
     PimcoreDataImporterBundle::class => ['all' => true],
     PimcoreDataHubBundle::class => ['all' => true],
+    PimcoreApplicationLoggerBundle::class => ['all' => true],
 ];
