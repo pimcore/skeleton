31b4ae468b50acd17f44469995c1b420722d86a9
Sebastian Blank <blank@data-factory.net>
2024-06-28 09:46:49 +0200

    Remove unnecessary php docs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,11 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends FrontendController
 {
-    /**
-     * @param Request $request
-     *
-     * @return Response
-     */
     public function defaultAction(Request $request): Response
     {
         return $this->render('default/default.html.twig');
