19320c41fb8906a2477ef32ae8759344d7a7510b
robertSt7 <robert.steinkellner@pimcore.com>
2024-12-10 15:43:36 +0100

    Task: add-quill to bundleSetup

--- src/EventSubscriber/BundleSetupSubscriber.php
+++ src/EventSubscriber/BundleSetupSubscriber.php
@@ -20,6 +20,7 @@ namespace App\EventSubscriber;
 use Pimcore\Bundle\AdminBundle\PimcoreAdminBundle;
 use Pimcore\Bundle\InstallBundle\Event\BundleSetupEvent;
 use Pimcore\Bundle\InstallBundle\Event\InstallEvents;
+use Pimcore\Bundle\QuillBundle\PimcoreQuillBundle;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BundleSetupSubscriber implements EventSubscriberInterface
@@ -37,5 +38,6 @@ class BundleSetupSubscriber implements EventSubscriberInterface
     {
         // add required PimcoreAdminBundle
         $event->addRequiredBundle('PimcoreAdminBundle', PimcoreAdminBundle::class, true);
+        $event->addInstallableBundle('PimcoreQuillBundle', PimcoreQuillBundle::class, true);
     }
 }
