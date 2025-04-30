b94d89d0a7b8f8155bbb167c7f380876a0367b79
Christian Fasching <christian.fasching@pimcore.com>
2025-04-30 06:51:35 +0200

    License Switch to POCL

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 namespace App\Controller;
 
 use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
