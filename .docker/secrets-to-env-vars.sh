e48151cd4aa55dd60a71e38856ac422577b22f75
Evan Jackson <evan.jackson@torqit.ca>
2026-01-19 16:21:52 -0500

    Revert changes to secrets-to-env-vars as they were unnecessary

--- .docker/secrets-to-env-vars.sh
+++ .docker/secrets-to-env-vars.sh
@@ -2,6 +2,6 @@
 
 # Certain secrets cause problems with Pimcore when used as files - this script converts them to env vars, and then containers can source this script at startup.
 # IMPORTANT: Only use this for secrets that must be converted to env vars. For all other secrets, continue to use them as files.
-export PIMCORE_ENCRYPTION_SECRET=${PIMCORE_ENCRYPTION_SECRET:-$(cat /run/secrets/pimcore-encryption-secret 2>/dev/null || echo "")}
-export PIMCORE_INSTANCE_IDENTIFIER=${PIMCORE_INSTANCE_IDENTIFIER:-$(cat /run/secrets/pimcore-instance-identifier 2>/dev/null || echo "")}
-export PIMCORE_PRODUCT_KEY=${PIMCORE_PRODUCT_KEY:-$(cat /run/secrets/pimcore-product-key 2>/dev/null || echo "")}
+export PIMCORE_ENCRYPTION_SECRET=$(cat /run/secrets/pimcore-encryption-secret 2>/dev/null || echo "")
+export PIMCORE_INSTANCE_IDENTIFIER=$(cat /run/secrets/pimcore-instance-identifier 2>/dev/null || echo "")
+export PIMCORE_PRODUCT_KEY=$(cat /run/secrets/pimcore-product-key 2>/dev/null || echo "")
