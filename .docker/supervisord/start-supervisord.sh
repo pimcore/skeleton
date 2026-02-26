d2cd20e6d689160d805fcdfc75351cb7463bbba1
Evan Jackson <evan.jackson@torqit.ca>
2026-02-26 15:54:54 -0500

    supervisorctl default setup

--- .docker/supervisord/start-supervisord.sh
+++ .docker/supervisord/start-supervisord.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /etc/profile.d/secrets-to-env-vars.sh
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/pimcore.conf
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
