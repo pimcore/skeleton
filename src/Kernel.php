514d279fc29124260da285e58ed38e71c69e6bb1
Marco Perberschlager <marco.perberschlager@pimcore.com>
2026-03-26 09:33:25 +0100

    Addressing review feedback

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,20 +12,8 @@
 
 namespace App;
 
-use Pimcore\Bundle\QuillBundle\PimcoreQuillBundle;
-use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 use Pimcore\Kernel as PimcoreKernel;
 
 class Kernel extends PimcoreKernel
 {
-    /**
-     * Adds bundles to register to the bundle collection. The collection is able
-     * to handle priorities and environment-specific bundles.
-     */
-    public function registerBundlesToCollection(BundleCollection $collection): void
-    {
-        if (class_exists(PimcoreQuillBundle::class)) {
-            $collection->addBundle(new PimcoreQuillBundle());
-        }
-    }
 }
